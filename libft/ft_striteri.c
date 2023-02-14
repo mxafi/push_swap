339231a18dea035c42e135d47b735b7dd4725f23
Markus Laaksonen <malaakso@student.hive.fi>
2023-02-14 18:45:59 +0200

    add libft

---
+++ libft/ft_striteri.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: malaakso <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/08 15:02:04 by malaakso          #+#    #+#             */
+/*   Updated: 2022/11/08 15:15:29 by malaakso         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+	size_t	i;
+	size_t	len;
+
+	if (!s)
+		return ;
+	len = ft_strlen(s);
+	i = 0;
+	while (s[i])
+	{
+		f((unsigned int)i, &s[i]);
+		i++;
+	}
+}
