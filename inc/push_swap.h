2dea4f7ac0034252887a8f053e4ab041d8bf7ce0
Markus Laaksonen <malaakso@student.hive.fi>
2023-03-02 14:28:16 +0200

    add psl functions to header

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: malaakso <malaakso@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 18:37:35 by malaakso          #+#    #+#             */
-/*   Updated: 2023/03/02 14:23:08 by malaakso         ###   ########.fr       */
+/*   Updated: 2023/03/02 14:27:51 by malaakso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,14 @@ void	read_args(t_push_swap *data, int ac, char **av);
 void	sa(t_push_swap *data);
 void	sb(t_push_swap *data);
 void	ss(t_push_swap *data);
+void	pa(t_push_swap *data);
+void	pb(t_push_swap *data);
+void	ra(t_push_swap *data);
+void	rb(t_push_swap *data);
+void	rr(t_push_swap *data);
+void	rra(t_push_swap *data);
+void	rrb(t_push_swap *data);
+void	rrr(t_push_swap *data);
 int		stack_push(int *stack, int size, int value);
 int		stack_pop(int *stack, int size);
 
