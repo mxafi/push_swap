9acac92319e751e616e3016944d227d64ff8792b
Markus Laaksonen <malaakso@student.hive.fi>
2023-03-02 15:02:36 +0200

    fix data malloc size, make print more verbose

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: malaakso <malaakso@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/14 18:37:41 by malaakso          #+#    #+#             */
-/*   Updated: 2023/03/02 12:13:48 by malaakso         ###   ########.fr       */
+/*   Updated: 2023/03/02 14:58:17 by malaakso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,13 @@ void	print_stacks(t_push_swap *data)
 	int	i;
 
 	i = 0;
-	ft_printf("Stack A: ");
+	ft_printf("Stack A (%i): ", data->stack_a_size);
 	while (i < data->stack_a_size)
 	{
 		ft_printf("%i ", data->stack_a[i]);
 		i++;
 	}
-	ft_printf("\nStack B: ");
+	ft_printf("\nStack B (%i): ", data->stack_b_size);
 	i = 0;
 	while (i < data->stack_b_size)
 	{
@@ -40,12 +40,12 @@ int	main(int ac, char **av)
 {
 	t_push_swap	*data;
 
-	data = malloc(sizeof(data));
+	data = malloc(sizeof(t_push_swap));
 	// handle malloc fail
 	is_valid_input(ac, av);
 	read_args(data, ac, av);
-	//run_algorithms(data); // todo: runs algos, chooses shortest, prints its commands
 	ft_printf("Passed validity checks\n");
 	print_stacks(data);
+	//run_algorithms(data); // todo: runs algos, chooses shortest, prints its commands
 	return (0);
 }
